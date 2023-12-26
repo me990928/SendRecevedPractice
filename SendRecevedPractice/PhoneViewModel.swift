725716e5f2bb519afc7ed9ea66e8c71c64b85d1c
me990928 <me990928@gmail.com>
2023-12-26 11:52:06 +0900

    iPhone側の初期化

--- SendRecevedPractice/PhoneViewModel.swift
+++ SendRecevedPractice/PhoneViewModel.swift
@@ -5,4 +5,30 @@
 //  Created by 広瀬友哉 on 2023/12/26.
 //
 
-import Foundation
+import WatchConnectivity
+
+class PhoneViewModel: NSObject, ObservableObject, WCSessionDelegate {
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        
+    }
+    
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+        
+    }
+    
+    
+    private let session: WCSession
+    
+    init(session: WCSession = .default) {
+        self.session = session
+        super.init()
+        self.session.delegate = self
+        self.session.activate()
+    }
+    
+}
