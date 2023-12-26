0c7107a7e143f000f05f15878d1d5c7efbe0ee6c
me990928 <me990928@gmail.com>
2023-12-26 16:12:18 +0900

    applewatchの送受信

--- SendRecevedPractice Watch App/WatchViewModel.swift
+++ SendRecevedPractice Watch App/WatchViewModel.swift
@@ -9,18 +9,64 @@ import SwiftUI
 import WatchConnectivity
 
 final class WatchViewModel: NSObject, ObservableObject, WCSessionDelegate {
+    
     func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
         
     }
     
+    func session(_ session: WCSession, didReceiveUserInfo userInfo: [String : Any] = [:]) {
+        Task {
+            @MainActor in
+            
+            if let value = userInfo["count"] as? Int {
+                counter = value
+            } else {
+                print("None")
+            }
+            
+            if let value = userInfo["message"] as? String {
+                message = value
+            }
+        }
+    }
+    
+    
     
     private let session: WCSession
+    
+    @Published var message: String = ""
+    
+    @Published var counter: Int = 0
     
     init(session: WCSession = .default) {
         self.session = session
         super.init()
         self.session.delegate = self
         self.session.activate()
+    }
+    
+    func countUp() {
+        counter += 1
+        sendImmediately()
+    }
+    
+    func sendImmediately(){
+        guard session.activationState == .activated else {
+            print("Sending method an only be called while the session is .activ")
+            return
+        }
+        
+        Task.detached(priority: .medium) { [self] in
+            
+            let message = [
+                "count": counter
+            ] as [String: Any]
+            
+            self.session.sendMessage(message, replyHandler: nil){ error in
+                print(error)
+            }
+            
+        }
     }
     
 }
