5dcc7016bef4940bbfc69baa724b6665affa9dfc
me990928 <me990928@gmail.com>
2023-12-26 12:12:13 +0900

    iPhone側の送信機能

--- SendRecevedPractice/PhoneViewModel.swift
+++ SendRecevedPractice/PhoneViewModel.swift
@@ -21,14 +21,41 @@ class PhoneViewModel: NSObject, ObservableObject, WCSessionDelegate {
         
     }
     
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+        Task { @MainActor in
+            if let value = message["msg"] as? String {
+                self.message = value
+            }
+        }
+    }
     
     private let session: WCSession
+    
+    @Published var message: String = ""
+    
+    @Published var counter: Int = 0
     
     init(session: WCSession = .default) {
         self.session = session
         super.init()
         self.session.delegate = self
         self.session.activate()
+    }
+    
+    func send(messages: String){
+        guard session.activationState == .activated else {
+            print("Sending method an only be called while the session is .activ")
+            return
+        }
+        
+        Task.detached(priority: .medium) { [self] in
+            
+            let userInfo = [
+                "msg": messages
+            ] as [String: Any]
+            
+            session.transferUserInfo(userInfo)
+        }
     }
     
 }
